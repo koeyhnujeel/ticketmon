a91635d872f48a330dfc8d66a30c232b1e0c1211
zunza <fksfks015@naver.com>
2025-02-14 16:32:49 +0900

    feat: AuthenticationEntryPoint 추가

---
+++ src/main/java/com/zunza/ticketmon/global/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,38 @@
+package com.zunza.ticketmon.global.security;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zunza.ticketmon.global.common.ErrorResponse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException authException) throws IOException, ServletException {
+
+		ErrorResponse errorResponse = ErrorResponse.builder()
+			.message("로그인이 필요한 서비스입니다.")
+			.code(HttpServletResponse.SC_UNAUTHORIZED)
+			.build();
+
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		objectMapper.writeValue(response.getWriter(), errorResponse);
+	}
+}
+
