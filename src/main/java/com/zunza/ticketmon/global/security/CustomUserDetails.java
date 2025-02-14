230c5c883a88b784bcc99617163743cb3d338c77
zunza <fksfks015@naver.com>
2025-02-14 16:35:59 +0900

    feat: CustomUserDetails 추가

---
+++ src/main/java/com/zunza/ticketmon/global/security/CustomUserDetails.java
@@ -0,0 +1,63 @@
+package com.zunza.ticketmon.global.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.zunza.ticketmon.domain.user.User;
+
+import lombok.Getter;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+	private final Long userId;
+	private final String email;
+	private final String password;
+	private final List<GrantedAuthority> authorities;
+
+	public CustomUserDetails(User user) {
+		this.userId = user.getId();
+		this.email = user.getEmail();
+		this.password = user.getPassword();
+		this.authorities = List.of(new SimpleGrantedAuthority("USER"));
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
