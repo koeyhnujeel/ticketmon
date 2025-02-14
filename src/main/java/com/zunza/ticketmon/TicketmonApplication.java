d21853ea5ecb70ebda238b55fc53c0f7a2ad3454
zunza <fksfks015@naver.com>
2025-02-14 16:41:37 +0900

    first commit

---
+++ src/main/java/com/zunza/ticketmon/TicketmonApplication.java
@@ -0,0 +1,13 @@
+package com.zunza.ticketmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zunza/ticketmon/TicketmonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zunza.ticketmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
