d380c54fcacef6403db8ab0aafce73e4117dae27
zunza <fksfks015@naver.com>
2025-02-24 17:26:13 +0900

    feat: Ticket 엔티티 클래스 추가

--- src/main/java/com/zunza/ticketmon/domain/ticket/Ticket.java
+++ src/main/java/com/zunza/ticketmon/domain/ticket/Ticket.java
@@ -1,14 +1,35 @@
-// package com.zunza.ticketmon.domain.ticket;
-//
-// import jakarta.persistence.Entity;
-// import lombok.AccessLevel;
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-//
-// @Getter
-// @Entity
-// @NoArgsConstructor(access = AccessLevel.PROTECTED)
-// public class Ticket {
-//
-//
-// }
+package com.zunza.ticketmon.domain.ticket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.zunza.ticketmon.domain.performance.entity.PerformanceSeat;
+import com.zunza.ticketmon.domain.user.User;
+import com.zunza.ticketmon.global.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ticket extends BaseEntity {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@OneToMany(mappedBy = "ticket")
+	private List<PerformanceSeat> performanceSeats = new ArrayList<>();
+
+	public Ticket(User user, List<PerformanceSeat> performanceSeats) {
+		this.user = user;
+		this.performanceSeats = performanceSeats;
+	}
+}
